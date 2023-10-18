29c3508e1acb6b5be83d1f849c8875981499f255
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 11:58:00 +0200

    Added "getfiletype" function

--- Enums/filetype.py
+++ Enums/filetype.py
@@ -9,3 +9,13 @@ class Filetype(Enum):
 
 
 #Filetype = Enum('Filetype', ['TXT', 'JPEG', 'PNG', 'UNKNOWN'])
+
+def getfiletype(s :str)-> Filetype:
+    if s == "TXT":
+        return Filetype.TXT
+    elif s == "JPEG":
+        return Filetype.JPEG
+    elif s == "PNG":
+        return Filetype.PNG
+    else:
+        return Filetype.UNKNOWN
