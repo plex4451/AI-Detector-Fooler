a89d99ce4077b710563445393a82ed1f6a07c2f7
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 12:23:35 +0200

    Added a simple leet-speak algorithm

---
+++ Algo/textalgo.py
@@ -0,0 +1,13 @@
+
+
+
+def changeText(txt: str) -> str:
+    txt = txt.replace("a", "4")
+    txt = txt.replace("A", "4")
+    txt = txt.replace("o", "0")
+    txt = txt.replace("O", "0")
+    txt = txt.replace("t", "7")
+    txt = txt.replace("T", "7")
+    txt = txt.replace("s", "5")
+    txt = txt.replace("S", "5")
+    return (txt)
