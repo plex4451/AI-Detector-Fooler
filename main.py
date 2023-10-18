6d2ccd47a22f15b80287a21adba9364e947620bb
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 12:03:14 +0200

    Added outputtype, inputtype and 2 Error Conditions

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import sys
+from Enums.filetype import *
 
-from Enums.filetype import Filetype
 
-x = Filetype.PNG
-
+print(Filetype.PNG)
 inputpath = sys.argv[1]
 outputpath = sys.argv[2]
+inputtype = getfiletype(inputpath.split('.')[-1])
+outputtype = getfiletype(outputpath.split('.')[-1])
+
+
+if((inputtype != outputtype) or ( (inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN) ) ):
+    if(inputtype != outputtype):
+        print("ERROR 001: Output-type and Input-type are different!")
+        exit(1)
+    else:
+        print("ERROR 002: Input-type or Output-type is not known to this Program!")
+        exit(2)
+else:
+    [...]
+
+
 
 print(inputpath)
 print(outputpath)
