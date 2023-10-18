0d9a741ff0f4abe1193dfc47e0d7bad9911012e7
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 12:20:17 +0200

    Added destination between text and image Algo.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from Enums.filetype import *
 
-
 inputpath = sys.argv[1]
 outputpath = sys.argv[2]
 inputtype = getfiletype(inputpath.split('.')[-1].upper())
@@ -9,12 +8,21 @@ outputtype = getfiletype(outputpath.split('.')[-1].upper())
 print("Filetype of the Input: {}".format(inputtype))
 print("Filetype of the Output: {}".format(outputtype))
 
-if((inputtype != outputtype) or ( (inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN) ) ):
-    if(inputtype != outputtype):
+# ERROR Handeling with mismatching Filetypes and UKNOWN Filetypes
+if ((inputtype != outputtype) or ((inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN))):
+    if (inputtype != outputtype):
         print("ERROR 001: Output-type and Input-type are mismatching!")
         exit(1)
     else:
         print("ERROR 002: Input-type or Output-type is not known to this Program!")
         exit(2)
 else:
-    print("Success!")
+    if(inputtype == Filetype.TXT):
+        print("Text-ALGO Missing.")
+        #TODO: INSERT ALGO FOR TEXT HERE
+
+    else:
+        print("Image-ALGO Missing")
+        #TODO: INSERT ALGO FOR IMAGE HERE
+
+
