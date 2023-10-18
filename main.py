99c4ab8ab4bbe422e18fe9324ab72bb6afce980d
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 11:35:03 +0200

    Added Import for the Enum "Filetype"

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 
+from Enums.filetype import Filetype
+
+x = Filetype.PNG
+
 inputpath = sys.argv[1]
 outputpath = sys.argv[2]
 
-
-
-
-
 print(inputpath)
-print (outputpath)
+print(outputpath)
