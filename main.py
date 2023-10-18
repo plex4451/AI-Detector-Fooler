2c22f612676681e6d4b334af4dbbc441a6b934c9
Alex Alfonso Trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-18 12:06:03 +0200

    Bugfixing and adding debug

--- main.py
+++ main.py
@@ -2,24 +2,19 @@ import sys
 from Enums.filetype import *
 
 
-print(Filetype.PNG)
 inputpath = sys.argv[1]
 outputpath = sys.argv[2]
-inputtype = getfiletype(inputpath.split('.')[-1])
-outputtype = getfiletype(outputpath.split('.')[-1])
-
+inputtype = getfiletype(inputpath.split('.')[-1].upper())
+outputtype = getfiletype(outputpath.split('.')[-1].upper())
+print("Filetype of the Input: {}".format(inputtype))
+print("Filetype of the Output: {}".format(outputtype))
 
 if((inputtype != outputtype) or ( (inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN) ) ):
     if(inputtype != outputtype):
-        print("ERROR 001: Output-type and Input-type are different!")
+        print("ERROR 001: Output-type and Input-type are mismatching!")
         exit(1)
     else:
         print("ERROR 002: Input-type or Output-type is not known to this Program!")
         exit(2)
 else:
-    [...]
-
-
-
-print(inputpath)
-print(outputpath)
+    print("Success!")
