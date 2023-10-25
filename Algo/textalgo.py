7d7335147cea5b93f2b7a718aefd186d4b982e8c
ale.alfonso_trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-10-25 12:15:32 +0200

    Refactoring of Methods

--- Algo/textalgo.py
+++ Algo/textalgo.py
@@ -5,7 +5,7 @@ max_percentage = 100
 
 
 #Debug-Info-Function: Prints information about the String
-def debugInfo(original_txt: str,final_txt: str):
+def debug_info(original_txt: str,final_txt: str):
     x = 0
     for i in range(0,len(original_txt)):
         if(original_txt[i]!=final_txt[i]):
@@ -15,20 +15,20 @@ def debugInfo(original_txt: str,final_txt: str):
 
 
 #Exchange-Char-Symbol: Replaces letters with similar symbols
-def changeLetterSymbol(txt: str) -> str:
+def change_letter_symbol(txt: str) -> str:
     txt = txt.replace("a","@")
     return(txt)
 
 
 
 # Invisible Char: Replaces Space with a invisible char
-def changeInvisibleChar(txt: str) -> str:
+def change_invisible_char(txt: str) -> str:
     txt = txt.replace(" ", " ")
     return (txt)
 
 
 # Leetspeak-Algo (replaces letters with numbers)
-def changeLeetspeak(txt: str) -> str:
+def change_leet_speak(txt: str) -> str:
     txt = txt.replace("a", "4")
     txt = txt.replace("A", "4")
     txt = txt.replace("o", "0")
@@ -40,12 +40,11 @@ def changeLeetspeak(txt: str) -> str:
     return txt
 
 #Main Methode to change txt (Deactivate certain Methods with a "#")
-def changeText(txt: str) -> str:
+def change_text(txt: str) -> str:
     original_txt = txt
-    txt = changeInvisibleChar(txt)
-    #txt = changeLetterSymbol(txt)
-    #txt = changeLeetspeak(txt)
-    debugInfo(original_txt,txt)
+    txt = change_invisible_char(txt)
+    #txt = change_letter_symbol(txt)
+    #txt = change_leet_speak(txt)
     return (txt)
 
 
