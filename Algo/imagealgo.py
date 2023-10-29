39f317e7e3014bf9c03e158e8da0a9e8c28c6d46
Lou Kielhorn <loukielhorn@gmail.com>
2023-10-29 08:42:10 +0100

    Add gaussian noise function as image algo.

---
+++ Algo/imagealgo.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def add_noise_to_image(image, mean=0, stddev=25):
+    # Load the image
+    image = image.astype(np.float32)
+
+    # Generate Gaussian noise with the same shape as the image
+    gaussian_noise = np.random.normal(mean, stddev, image.shape).astype(np.float32)
+
+    # Add the Gaussian noise to the image
+    noisy_image = cv2.add(image, gaussian_noise)
+    noisy_image = np.clip(noisy_image, 0, 255)
+    noisy_image = noisy_image.astype(np.uint8)
+    return noisy_image
+
