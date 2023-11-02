0204e2d47a68d7c9daca238d8d606552f5377fc3
ale.alfonso_trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-11-02 11:23:05 +0100

    Changed "Fenster" to "Window", geometry to 900x900, and changed title to "AI-NOOBS-UNLEASHED: AI FOOLER"

--- GUI/gui.py
+++ GUI/gui.py
@@ -4,18 +4,18 @@ from tkinter import *
 
 #Initialise
 def open_window():
-    # Fenster
-    Fenster = Tk()
-    Fenster.geometry('300x300')
-    Fenster.title('Verschlüsseln')
-    Fenster.resizable(False, False)
+    # Window
+    Window = Tk()
+    Window.geometry('900x900')
+    Window.title('AI-NOOBS-UNLEASHED: AI FOOLER')
+    Window.resizable(False, False)
 
     # Label
-    LabelW = Label(Fenster, text='Wort:')
-    LabelWV = Label(Fenster, text='Wort Verschlüsselt:')
-    LabelWVD = Label(Fenster, text='N/A')
-    LabelK = Label(Fenster, text='Key:')
-    LabelKW = Label(Fenster, text='Word Key:')
+    LabelW = Label(Window, text='Wort:')
+    LabelWV = Label(Window, text='Wort Verschlüsselt:')
+    LabelWVD = Label(Window, text='N/A')
+    LabelK = Label(Window, text='Key:')
+    LabelKW = Label(Window, text='Word Key:')
 
     LabelW.place(x=10, y=20, width=60, height=50)
     LabelWV.place(x=10, y=200, width=150, height=50)
@@ -24,19 +24,19 @@ def open_window():
     LabelKW.place(x=85, y=70, width=80, height=50)
 
     # Entry
-    Entry1 = Entry(Fenster)
-    Entry2 = Entry(Fenster)
-    Entry3 = Entry(Fenster)
+    Entry1 = Entry(Window)
+    Entry2 = Entry(Window)
+    Entry3 = Entry(Window)
     Entry1.place(x=20, y=55, width=60, height=25)
     Entry2.place(x=20, y=110, width=60, height=25)
     Entry3.place(x=90, y=110, width=60, height=25)
 
     # Listbox
-    Listbox1 = Listbox(Fenster)
+    Listbox1 = Listbox(Window)
     Listbox1.place(x=170, y=50, width=100, height=200)
 
     # Scrollbar
-    Scrollbar1 = Scrollbar(Fenster)
+    Scrollbar1 = Scrollbar(Window)
     Scrollbar1.place(x=270, y=50, width=15, height=200)
 
     # Config
@@ -44,12 +44,12 @@ def open_window():
     Scrollbar1.config(command=Listbox1.yview)
 
     # Button
-    Button1 = Button(Fenster, text='Codieren')
+    Button1 = Button(Window, text='Codieren')
     Button1.place(x=20, y=150, width=70, height=25)
 
-    Button2 = Button(Fenster, text='Decodieren')
+    Button2 = Button(Window, text='Decodieren')
     Button2.place(x=20, y=180, width=80, height=25)
 
-    Button3 = Button(Fenster, text='Codieren W')
+    Button3 = Button(Window, text='Codieren W')
     Button3.place(x=90, y=150, width=80, height=25)
-    Fenster.mainloop()
+    Window.mainloop()
