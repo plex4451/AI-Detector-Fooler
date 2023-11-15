9877c243eaeaf584c1d8cee5d29135e7a7f27d25
ale.alfonso_trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-11-15 10:19:10 +0100

    Moved the threading from ui to main. Added some print information

--- main.py
+++ main.py
@@ -23,49 +23,56 @@ def test_txt(txt: str):
 
 
 
+def main_method():
+    # ERROR Handeling with mismatching Filetypes and UKNOWN Filetypes
+    if ((inputtype != outputtype) or ((inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN))):
+        if (inputtype != outputtype):
+            print("ERROR 001: Output-type and Input-type are mismatching!")
+            exit(1)
+        else:
+            print("ERROR 002: Input-type or Output-type is not known to this Program!")
+            exit(2)
+    else:
+        if(inputtype == Filetype.TXT):
+            file = open(inputpath,"r",encoding="utf-8")
+            text = file.read()
+            file.close()
+            final_text = change_text(text)
 
-#debug: Enables/Disables Debug prints and Methods for Testing
+            file = open(outputpath, "x", encoding='utf-8')
+            file.write(final_text)
+            file.close()
+
+            if(debug):
+                print("----------------------------------------------------------------------------")
+                #test_txt(final_text)
+                print("test_txt is disabled!")
+                print("----------------------------------------------------------------------------")
+                debug_info_text(text, final_text)
+                print("----------------------------------------------------------------------------")
+                print(final_text)
+                print("----------------------------------------------------------------------------")
+            print("Text-ALGO Complete")
+        else:
+            print("Image-ALGO Missing")
+            #TODO: INSERT ALGO FOR IMAGE HERE
+
+
+
+# MAIN-METHOD
+
+# debug: Enables/Disables Debug prints and Methods for Testing
 debug = True
-gui_thread = threading.Thread(target = open_window_thread())
-gui_thread.start()
 inputpath = sys.argv[1]
 outputpath = sys.argv[2]
 inputtype = getfiletype(inputpath.split('.')[-1].upper())
 outputtype = getfiletype(outputpath.split('.')[-1].upper())
+print("----------------------------------------------------------------------------")
 print("Filetype of the Input: {}".format(inputtype))
 print("Filetype of the Output: {}".format(outputtype))
-
-# ERROR Handeling with mismatching Filetypes and UKNOWN Filetypes
-if ((inputtype != outputtype) or ((inputtype == Filetype.UNKNOWN) or (outputtype == Filetype.UNKNOWN))):
-    if (inputtype != outputtype):
-        print("ERROR 001: Output-type and Input-type are mismatching!")
-        exit(1)
-    else:
-        print("ERROR 002: Input-type or Output-type is not known to this Program!")
-        exit(2)
-else:
-    if(inputtype == Filetype.TXT):
-        file = open(inputpath,"r",encoding="utf-8")
-        text = file.read()
-        file.close()
-        final_text = change_text(text)
-
-        file = open(outputpath, "x", encoding='utf-8')
-        file.write(final_text)
-        file.close()
-
-        if(debug):
-            print("----------------------------------------------------------------------------")
-            #test_txt(final_text)
-            print("----------------------------------------------------------------------------")
-            debug_info_text(text, final_text)
-            print("----------------------------------------------------------------------------")
-            print(final_text)
-            print("----------------------------------------------------------------------------")
-        print("Text-ALGO Missing.")
-        #TODO: INSERT ALGO FOR TEXT HERE
-
-    else:
-        print("Image-ALGO Missing")
-        #TODO: INSERT ALGO FOR IMAGE HERE
-    print("Success!")
+print("----------------------------------------------------------------------------")
+main_thread = threading.Thread(target=main_method())
+main_thread.start()
+open_window()
+main_thread.join()
+print("Success!")
