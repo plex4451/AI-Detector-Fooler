f92bf398324269d1b07cd0dc1d645ed347a91111
Lou Kielhorn <loukielhorn@gmail.com>
2023-11-29 10:34:10 +0100

    Added function to apply a simple/lighter sharpening to an image

--- Algo/imagealgo.py
+++ Algo/imagealgo.py
@@ -35,7 +35,20 @@ def make_dark_pixels_brighter(image, brightness_increase=10, threshold=20):
     return image
 
 
-def apply_sharpening(image):
+def light_sharpening(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
+
+def extreme_sharpening(image):
     # Warning use with caution extreme sharping!
     # Create a sharpening kernel
     kernel = np.array([[-1, -1, -1],
