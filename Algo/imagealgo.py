a2bb63ae696a5bc0b7f266ad053baf42ab8a8821
ale.alfonso_trigo <alex.alfonso.trigo@stud.uni-hannover.de>
2023-12-06 11:23:48 +0100

    Deactivated add_copyright method

--- Algo/imagealgo.py
+++ Algo/imagealgo.py
@@ -204,7 +204,7 @@ def use_alog_on_image(image):
     modified_image = add_noise_to_image(modified_image, 10, 20)
     modified_image = add_white_brush_with_alpha(modified_image)
     modified_image = light_sharpening(modified_image)
-    #modified_image = add_copyright_text(modified_image)
+    # modified_image = add_copyright_text(modified_image)
     # modified_image = add_blur_to_image(modified_image, 5)
     return modified_image
 
