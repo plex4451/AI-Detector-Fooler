f58e8e555d3535d61eb002235893fbb649828653
Alex <alexalfonsotrigo@gmail.com>
2024-01-14 21:17:29 +0100

    Code Cleaned for main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import cv2
 def test_txt(txt: str):
     points_array = get_scores_txt(txt)
     detector_list = ["Grammica.com", "Scribbr.com", "Detecting-ai.com Methode-A", "Detecting-ai.com Methode-B",
-                     "Writer.com", "UKNOWN", "UKNOWN", "UKNOWN", "UKNOWN", "UKNOWN"]
+                     "Writer.com"]
     counter = 0
     for i in points_array:
         print(("{name:} score: {score:}%".format(name=detector_list[counter], score=i)))
@@ -22,7 +22,12 @@ def test_txt(txt: str):
 
 
 def main_method():
-    # ERROR Handeling with mismatching Filetypes (not the sam) and UKNOWN Filetypes
+    """
+    This is the main method of the program that converts the input file to the output file from the given arguments
+    Returns:
+        None
+    """
+    # ERROR Handling with mismatching Filetypes (not the sam) and UKNOWN Filetypes
     if ((input_type == Filetype.TXT or output_type == Filetype.TXT) and (output_type != input_type)) or (
             (input_type == Filetype.UNKNOWN) or (output_type == Filetype.UNKNOWN)):
         if input_type == Filetype.UNKNOWN or output_type == Filetype.UNKNOWN:
@@ -34,17 +39,24 @@ def main_method():
 
     else:
         if input_type == Filetype.TXT:
+            """
+            This is the main method for the Text-Algo
+            """
             start_time = time.time()
+
             file = open(input_path, "r", encoding="utf-8")
             text = file.read()
             file.close()
+
             final_text = change_text(text)
+
             try:
                 file = open(output_path, "x", encoding='utf-8')
             except:
                 file = open(output_path, "w", encoding='utf-8')
             file.write(final_text)
             file.close()
+
             end_time = time.time()
             elapsed_time = end_time - start_time
 
@@ -52,7 +64,7 @@ def main_method():
             print("Text Algo used in: ", elapsed_time)
             print("----------------------------------------------------------------------------")
 
-            if (debug):
+            if debug:
                 print("----------------------------------------------------------------------------")
                 # test_txt(final_text)
                 print("test_txt is disabled!")
@@ -61,9 +73,12 @@ def main_method():
                 print("----------------------------------------------------------------------------")
                 print(final_text)
                 print("----------------------------------------------------------------------------")
-            print("Text-ALGO Completed")
+            print("Text-ALGO Completed!")
 
         else:
+            """
+            This is the main method for the Image-Algo
+            """
             start_time = time.time()
             image = cv2.imread(input_path)
             final_image = use_alog_on_image(image)
@@ -77,26 +92,33 @@ def main_method():
             print("Image-Algo Completed!")
 
 
-# MAIN-METHOD
 
+"""
+This is the main method of the program
+"""
 # debug: Enables/Disables Debug prints and Methods for Testing
-debug = True
-# ERROR Handleing with missing arguments
+debug = False
+# ERROR Handling with missing arguments
 try:
     input_path = sys.argv[1]
     output_path = sys.argv[2]
 except:
     print("ERROR 003: No given Arguments! The program needs two arguments -> main.py <inputpath> <outputpath>")
     exit(3)
+
 input_type = get_file_type(input_path.split('.')[-1].upper())
 output_type = get_file_type(output_path.split('.')[-1].upper())
+
 print("----------------------------------------------------------------------------")
 print("Filetype of the Input: {}".format(input_type))
 print("Filetype of the Output: {}".format(output_type))
 print("----------------------------------------------------------------------------")
+
 main_thread = threading.Thread(target=main_method())
 main_thread.start()
+
 app = Window()
 app.mainloop()
+
 main_thread.join()
 print("Success!")
